9e0de189bd46b6577008d11907dab1985b8a778e
Nikita <niki.kudryavcev@gmail.com>
2024-04-18 19:59:34 +0300

    Simple TODO list

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.css";
+
+interface TodoItem {
+  id: number;
+  text: string;
+  completed: boolean;
+}
 
 function App() {
+  const [tasks, setTasks] = useState<TodoItem[]>([]);
+  const [newTask, setNewTask] = useState("");
+
+  const addTask = () => {
+    if (newTask !== "") {
+      const newItem = {
+        id: Math.floor(Math.random() * (10000 - 1 + 1)) + 1,
+        text: newTask,
+        completed: false,
+      };
+      setTasks([...tasks, newItem]);
+      setNewTask("");
+    } else {
+      alert("Поле для задачи не может быть пустым!");
+    }
+  };
+
+  const removeTask = (id: number) => {
+    const modified = tasks.filter((task) => task.id !== id);
+    setTasks(modified);
+  };
+
+  const confirmTask = (id: number) => {
+    const modified = [...tasks];
+    for (let i = 0; i < modified.length; i++) {
+      if (modified[i].id === id) {
+        modified[i].completed = !modified[i].completed;
+      }
+    }
+    // modified.forEach((task) =>
+    //   task.id === id ? (task.confirmed = true) : task.confirmed
+    // );
+    setTasks(modified);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto max-w-3xl pt-5 pl-20 pr-20">
+      <form className="w-full ">
+        <div className="flex items-center py-2">
+          <input
+            className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+            type="text"
+            value={newTask}
+            placeholder="Введите задачу"
+            onChange={(e) => setNewTask(e.target.value)}
+          />
+          <button
+            className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+            type="button"
+            onClick={() => addTask()}
+          >
+            ADD TO TODO LIST
+          </button>
+        </div>
+      </form>
+      <div className="font-bold py-2 px-4 flex flex-col items-center mb-2">
+        TO DOs:
+      </div>
+      <div className="flex flex-col items-center">
+        {tasks.map((task) => (
+          <div
+            key={task.id}
+            className={
+              task.completed
+                ? "flex flex-row w-full mb-5 border-2 px-2 py-2 border-green-300"
+                : "flex flex-row w-full mb-5 border-2 px-2 py-2"
+            }
+          >
+            <div className="grow">{task.text}</div>
+            <div
+              className="mr-2 text-green-500"
+              onClick={() => confirmTask(task.id)}
+            >
+              ✓
+            </div>
+            <div
+              key={task.id}
+              className="grow-0 text-red-600"
+              onClick={() => removeTask(task.id)}
+            >
+              X
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
